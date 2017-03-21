374e397dce9dfb0a5c71645824dc120a4cc70802
Daniel <daniel_kish@outlook.com>
2017-03-21 10:54:14 +0300

    Cleanup

--- QuadEdge/QuadEdge.h
+++ QuadEdge/QuadEdge.h
@@ -3,105 +3,88 @@
 #include <array>
 
 template <class T>
-class QuadEdgeList {
+class QuadEdgeList 
+{
 public:
 	class QuadEdge;
-private:
+	class EdgeRef;
 	using List = std::list<QuadEdge>;
 	using QuadEdgeRef = typename List::iterator;
 
-public:
-	class EdgeRef {
-		QuadEdgeRef ptr;
-		int n;
-
-		friend QuadEdgeList;
-		friend void splice(EdgeRef a, EdgeRef b) {
-			auto alpha = a.Onext().Rot();
-			auto beta = b.Onext().Rot();
-
-			a.ptr->recs[a.n];
-			std::swap(a.ptr->recs[a.n].next, b.ptr->recs[b.n].next);
-			std::swap(alpha.ptr->recs[alpha.n].next, beta.ptr->recs[beta.n].next);
-		}
-	public:
-		EdgeRef(QuadEdgeRef, int);
-		EdgeRef();
-		T& data();
-		EdgeRef Onext();
-		EdgeRef Rot();
-		EdgeRef InvRot();
-		EdgeRef Sym();
-		EdgeRef Lnext();
-		EdgeRef Oprev();
-		EdgeRef Rprev();
-		bool operator==(EdgeRef const& other);
-		bool operator!=(EdgeRef const& other);
-	};
-
-	class QuadEdge { 
-	public:
-		struct Record {
-			EdgeRef next;
-			T data;
-		};
-		friend void splice(EdgeRef a, EdgeRef b);
-	private:
-		std::array<Record,4> recs[4];
-	public:
-		QuadEdge();
-		std::array<Record, 4> const& records() const {
-			return records;
-		}
-	};
-	
+	EdgeRef makeEdge();
+	void deleteEdge(EdgeRef);
+	std::size_t size() const;
+	QuadEdgeRef begin();
+	QuadEdgeRef end();
 private:
 	List quadEdges;
-public:
-	EdgeRef makeEdge();
-	void deleteEdge(EdgeRef e);
-	std::size_t size();
+};
 
-	typename List::iterator begin() {
-		return quadEdges.begin();
-	}
-	typename List::iterator end() {
-		return quadEdges.end();
+template <class T>
+class QuadEdgeList<T>::EdgeRef
+{
+	typename QuadEdgeList<T>::QuadEdgeRef qref;
+	int n;
+public:
+	EdgeRef(typename QuadEdgeList<T>::QuadEdgeRef, int);
+	EdgeRef();
+	T& data();
+	EdgeRef Onext();
+	EdgeRef Rot();
+	EdgeRef InvRot();
+	EdgeRef Sym();
+	EdgeRef Lnext();
+	EdgeRef Oprev();
+	EdgeRef Rprev();
+	bool operator==(EdgeRef const& other);
+	bool operator!=(EdgeRef const& other);
+	
+	friend QuadEdgeList;
+	friend void splice(typename QuadEdgeList<T>::EdgeRef a, typename QuadEdgeList<T>::EdgeRef b)
+	{
+		auto alpha = a.Onext().Rot();
+		auto beta = b.Onext().Rot();
+		std::swap(a.qref->recs[a.n].next, b.qref->recs[b.n].next);
+		std::swap(alpha.qref->recs[alpha.n].next, beta.qref->recs[beta.n].next);
 	}
 };
 
 template <class T>
-typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::makeEdge()
+class QuadEdgeList<T>::QuadEdge
 {
-	quadEdges.push_back(QuadEdge{});
-	QuadEdgeRef qref = std::prev(quadEdges.end());
-	qref->recs[0] = {{qref,0},T{}};
-	qref->recs[1] = {{qref,3},T{}};
-	qref->recs[2] = {{qref,2},T{}};
-	qref->recs[3] = {{qref,1},T{}};
-	return EdgeRef(qref, 0);
-}
+public:
+	struct Record {
+		typename QuadEdgeList<T>::EdgeRef next;
+		T data;
+	};
+	friend QuadEdgeList<T>;
+	friend QuadEdgeList<T>::EdgeRef;
+	friend void splice(typename QuadEdgeList<T>::EdgeRef a, typename QuadEdgeList<T>::EdgeRef b);
+private:
+	std::array<Record, 4> recs;
+public:
+	QuadEdge();
+	std::array<Record, 4> const& records() const;
+};
 
+
+// QuadEdge
 template <class T>
-void QuadEdgeList<T>::deleteEdge(typename QuadEdgeList<T>::EdgeRef e)
-{
-	splice(e, e.Oprev());
-	splice(e.Sym(), e.Sym().Oprev());
-	quadEdges.erase(e.ptr);
-}
-
-template <class T>
-std::size_t QuadEdgeList<T>::size()
-{
-	return quadEdges.size();
-}
-
-template<class T>
 QuadEdgeList<T>::QuadEdge::QuadEdge()
-{}
+{
+}
 
 template <class T>
-QuadEdgeList<T>::EdgeRef::EdgeRef(QuadEdgeRef qref, int n_) : ptr{qref}, n{n_}
+std::array<typename QuadEdgeList<T>::QuadEdge::Record, 4> const&
+QuadEdgeList<T>::QuadEdge::records() const
+{
+	return recs;
+}
+
+// EdgeRef 
+template <class T>
+QuadEdgeList<T>::EdgeRef::EdgeRef(typename QuadEdgeList<T>::QuadEdgeRef qref_, int n_)
+	: qref{qref_}, n{n_}
 {}
 
 template <class T>
@@ -111,56 +94,102 @@ QuadEdgeList<T>::EdgeRef::EdgeRef()
 template <class T>
 T& QuadEdgeList<T>::EdgeRef::data()
 {
-	return ptr->recs[n].data;
-}
-
-
-// EdgeRef ifc
-template <class T>
-typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Onext() {
-	return ptr->recs[n].next;
+	return qref->recs[n].data;
 }
 
 template <class T>
-typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Rot() {
-	EdgeRef copy = *this;
+typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Onext()
+{
+	return qref->recs[n].next;
+}
+
+template <class T>
+typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Rot()
+{
+	typename QuadEdgeList<T>::EdgeRef copy = *this;
 	copy.n = (copy.n + 1) % 4;
 	return copy;
 }
 
 template <class T>
-typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::InvRot() {
-	EdgeRef copy = *this;
+typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::InvRot()
+{
+	typename QuadEdgeList<T>::EdgeRef copy = *this;
 	copy.n = (copy.n + 3) % 4;
 	return copy;
 }
 
 template <class T>
-typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Sym() {
-	return (*this).Rot().Rot();
+typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Sym()
+{
+	typename QuadEdgeList<T>::EdgeRef copy = *this;
+	copy.n = (copy.n + 2) % 4;
+	return copy;
 }
 
 template <class T>
-typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Lnext() {
+typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Lnext()
+{
 	return this->InvRot().Onext().Rot();
 }
 
 template <class T>
-typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Oprev() {
+typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Oprev()
+{
 	return this->Rot().Onext().Rot();
 }
 
 template <class T>
-typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Rprev() {
+typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::EdgeRef::Rprev()
+{
 	return this->Sym().Onext();
 }
 
 template <class T>
 bool QuadEdgeList<T>::EdgeRef::operator==(typename QuadEdgeList<T>::EdgeRef const& other) {
-	return this->ptr == other.ptr && this->n == other.n;
+	return this->qref == other.qref && this->n == other.n;
 }
 
 template <class T>
 bool QuadEdgeList<T>::EdgeRef::operator!=(typename QuadEdgeList<T>::EdgeRef const& other) {
 	return !(*this == other);
+}
+
+// QuadEdgeList
+template <class T>
+typename QuadEdgeList<T>::EdgeRef QuadEdgeList<T>::makeEdge()
+{
+	quadEdges.push_back(QuadEdge());
+	QuadEdgeRef qref = std::prev(quadEdges.end());
+	qref->recs[0] = {EdgeRef(qref,0),T{}};
+	qref->recs[1] = {EdgeRef(qref,3),T{}};
+	qref->recs[2] = {EdgeRef(qref,2),T{}};
+	qref->recs[3] = {EdgeRef(qref,1),T{}};
+	return EdgeRef(qref, 0);
+}
+
+template<class T>
+void QuadEdgeList<T>::deleteEdge(typename QuadEdgeList<T>::EdgeRef e)
+{
+	splice(e, e.Oprev());
+	splice(e.Sym(), e.Sym().Oprev());
+	quadEdges.erase(e.qref);
+}
+
+template<class T>
+std::size_t QuadEdgeList<T>::size() const
+{
+	return quadEdges.size();
+}
+
+template<class T>
+typename QuadEdgeList<T>::QuadEdgeRef QuadEdgeList<T>::begin()
+{
+	return quadEdges.begin();
+}
+
+template<class T>
+typename QuadEdgeList<T>::QuadEdgeRef QuadEdgeList<T>::end()
+{
+	return quadEdges.end();
 }
--- QuadEdge/main.cpp
+++ QuadEdge/main.cpp
@@ -1,33 +1,24 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
-#include "Header.h"
+#include "QuadEdge.h"
+#include "boost\variant.hpp"
 
-//
-//struct Point { double x, y; };
-//
-//std::ostream& operator<<(std::ostream& os, Point p)
-//{
-//	return os << '(' << p.x << ' ' << p.y << ')';
-//}
-//
-//using Data = boost::variant<Point*, std::string*>;
-//
 
 int main()
 {
 	QuadEdgeList<int> list;
 
-	QuadEdgeList<int>::EdgeRef e;
-	e = list.makeEdge();
 	list.makeEdge();
+	auto e = list.makeEdge();
 
+	list.deleteEdge(e);
 
 	std::cout << list.size() << '\n';
 
 	for (QuadEdgeList<int>::QuadEdgeRef i = list.begin(); i != list.end(); ++i)
 	{
 		QuadEdgeList<int>::EdgeRef e(i, 0);
-		std::cout << e.data() << '\n';
+		std::cout << e.data() << ' ' << e.Sym().data() << '\n';
 	}
 	for (QuadEdgeList<int>::QuadEdge const& q : list)
 	{
